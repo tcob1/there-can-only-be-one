8ae03b9d066a3a6111aabdb5e83fce9c4647c741
Adam-Cisneros <adamcisneros2003@gmail.com>
2026-02-25 01:08:09 -0800

    added a HealthManger for damage and death and implemented damage dealing for ranged and melee weapons

--- Assets/Scripts/Interactables/Items/Usable.cs
+++ Assets/Scripts/Interactables/Items/Usable.cs
@@ -3,11 +3,32 @@ using UnityEngine;
 
 public class Usable : Weapon
 {
+    void Update()
+    {
+        if (isHeld)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Attack();
+            }
+        }
+    }
+
     public override void Attack()
     {
         if (!CanUse())
         {
             return;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 1.5f))
+        {
+            HealthManager victimHealth = hit.collider.GetComponent<HealthManager>();
+            if (victimHealth != null)
+            {
+                victimHealth.TakeDamage(GetDamage());
+            }
         }
 
         lastUseTime = Time.time;
