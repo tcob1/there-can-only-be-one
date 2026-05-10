fa15dbeb3ebd9e8853650d7bf3480669b4129b91
Lucas Hom <lucasrylanhom@gmail.com>
2026-05-10 04:58:43 -0700

    safe animation tree

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,21 +1,33 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System.Collections;
 
 public class MainMenuUI : MonoBehaviour
 {
     [SerializeField] private string gameSceneName = "Main";
+    [SerializeField] private GameObject safe;
 
     private void Start()
     {
         SFXManager.Instance.PlayLoopingMusic("Anticipation", 0f, 0.8f);
+
     }
 
     public void OnPlay()
     {
+
+        Animator animator = safe.GetComponent<Animator>();
+        animator.SetTrigger("SpinOpen");
+        StartCoroutine(LoadAfterAnimation(animator));
+    }
+
+    private IEnumerator LoadAfterAnimation(Animator animator)
+    {
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName("USE-THIS-ANIMATION"));
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName("Open"));
         SFXManager.Instance.StopLoopingMusic();
         SceneManager.LoadScene(gameSceneName);
-        
     }
 
     public void OnQuit()
