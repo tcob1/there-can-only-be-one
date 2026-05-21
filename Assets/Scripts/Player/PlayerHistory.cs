f5de51402ae1af4ea4e16b9716cb6fb7c0f00b64
Jacob Kelleran <jacob.kelleran@outlook.com>
2026-05-20 17:30:25 -0700

    Duplicate players now spawn/despawn properly

--- Assets/Scripts/Player/PlayerHistory.cs
+++ Assets/Scripts/Player/PlayerHistory.cs
@@ -38,12 +38,12 @@ public class PlayerHistory : MonoBehaviour
     private List<PlayerActionHistoryEntry> actionHistory;
     private long currentTime;
     private bool isActivePlayer = true;
-
-    public bool isReplaying { get; private set; }
+    private bool isOutsideOfTimeRange = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Debug.Log("Start called!");
         movementHistory = new();
         actionHistory = new();
         currentTime = 0;
@@ -53,17 +53,33 @@ public class PlayerHistory : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (isReplaying)
+        if (isActivePlayer)
         {
-            // find the current time in the history and set the player's position and rotation to match
-            // if the current time is past the end of the history, stop replaying
-            // if the current time is before the start of the history, do nothing (TODO: should deactivate player)
+            // record the player's position and rotation in the history
+            movementHistory.Add(new PlayerMovementHistoryEntry(transform.position, transform.rotation, lookTransform.rotation));
+        }
+        else
+        {
+            // Player movement is currently being replayed
+            // Find the current time in the history and set the player's
+            // position and rotation to match.
+            // If the current time is past the end of the history, the player
+            // time travelled back at this point, so despawn them.
+            // If the current time is before the start of the history, the
+            // player has not yet time traveled back to this point, so despawn
+            // them. They will become active in later loops.
             if (currentTime > movementHistory.Count - 1)
             {
-                StopReplay();
+                isOutsideOfTimeRange = true;
+                Despawn();
             }
             else if (currentTime >= 0)
             {
+                if (isOutsideOfTimeRange)
+                {
+                    // Player has just entered the replay zone and should be spawned in
+                    Spawn();
+                }
                 PlayerMovementHistoryEntry entry = movementHistory[(int)currentTime];
                 if (entry != null)
                 {
@@ -73,30 +89,20 @@ public class PlayerHistory : MonoBehaviour
                 }
             }
         }
-        else
-        {
-            // record the player's position and rotation in the history
-            movementHistory.Add(new PlayerMovementHistoryEntry(transform.position, transform.rotation, lookTransform.rotation));
-        }
 
         currentTime++;
     }
 
     void RecordAction(string actionName)
     {
-        actionHistory.Add(new PlayerActionHistoryEntry(actionName, TimeHub.Instance.getTime()));
+        actionHistory.Add(new PlayerActionHistoryEntry(actionName, currentTime));
     }
 
     void StartReplay()
     {
-        isReplaying = true;
+        isActivePlayer = false;
         mouseLook.isActive = false;
         playerMovement.isActive = false;
-    }
-
-    void StopReplay()
-    {
-        isReplaying = false;
     }
 
     void OnTimeTravel(int delta, long newTime)
@@ -108,8 +114,19 @@ public class PlayerHistory : MonoBehaviour
             CreateDuplicate();
             currentTime += delta * (long)(1.0f / Time.fixedDeltaTime);
             StartReplay();
+            if (currentTime < 0)
+            {
+                // before the player spawned in
+                isOutsideOfTimeRange = true;
+                Despawn();
+            }
+            else if (isOutsideOfTimeRange && currentTime < movementHistory.Count)
+            {
+                // the player was past the time range but has moved back into it
+                isOutsideOfTimeRange = false;
+                Spawn();
+            }
         }
-        // TODO: is this it???
     }
 
     void CreateDuplicate()
@@ -121,7 +138,26 @@ public class PlayerHistory : MonoBehaviour
             GameObject duplicate = Instantiate(gameObject, transform.position, transform.rotation);
             // destroy the camera so it uses the duplicate's camera, which is the new main player
             Destroy(playerCamera);
-            isActivePlayer = false;
+        }
+    }
+
+    void Despawn()
+    {
+        // Deactivate all children
+        // Cannot deactivate self because that would stop this script from running,
+        // and this script needs to re-spawn the player.
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    void Spawn()
+    {
+        // Reactivate children, will add effects later
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
         }
     }
 }
